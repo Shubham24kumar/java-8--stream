4052dba704d2dabeb5ab22e80f66f6ea330a2f0f
Shubham Sharma <Shubham Sharma@DESKTOP-F18I2GL>
2022-04-21 11:10:00 +0530

    basic eclipse project

---
+++ GitHub_Project/src/main/java/com/github_Example/Addition.java
@@ -0,0 +1,13 @@
+package com.github_Example;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a= 10;
+		int b =5;
+		int c = a+b;
+		 System.out.println("addition of two numbers is : " + c);
+
+	}
+
+}
